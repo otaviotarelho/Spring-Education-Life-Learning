dfb0edf105e2591f55f6cc7b2d964e33e6b28ae0
otaviotarelho <otarelho@ucdavis.edu>
2018-08-18 01:28:20 -0300

    UserService and UserServiceTest

--- src/main/java/com/otaviotarelho/springrestcrud/services/UserService.java
+++ src/main/java/com/otaviotarelho/springrestcrud/services/UserService.java
@@ -1,8 +1,32 @@
 package com.otaviotarelho.springrestcrud.services;
 
+import com.otaviotarelho.springrestcrud.domains.User;
+import com.otaviotarelho.springrestcrud.repositories.UserRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    public UserRepository userRepository;
+
+    public User findUserById(Integer id){
+        return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(new User(),"User not found"));
+    }
+
+    public User insert(User obj){
+        obj.setId(null);
+        return userRepository.save(obj);
+    }
+
+    public User update(User obj){
+        return userRepository.save(obj);
+    }
+
+    public void delete(Integer id){
+        User user = findUserById(id);
+        userRepository.delete(user);
+    }
 }
--- src/test/java/com/otaviotarelho/springrestcrud/services/UserServiceTest.java
+++ src/test/java/com/otaviotarelho/springrestcrud/services/UserServiceTest.java
@@ -1,7 +1,61 @@
 package com.otaviotarelho.springrestcrud.services;
 
-import static org.junit.Assert.*;
+import com.otaviotarelho.springrestcrud.domains.User;
+import com.otaviotarelho.springrestcrud.repositories.UserRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 public class UserServiceTest {
 
+    private UserRepository repository;
+    private UserService service;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void init(){
+        repository = Mockito.mock(UserRepository.class);
+        service = new UserService();
+    }
+
+    @Test
+    public void shouldThrowExceptionIfUserNotFound(){
+        Mockito.when(repository.findByEmail(getUser().getEmail())).thenReturn(null);
+
+        exception.expect(ObjectNotFoundException.class);
+        exception.expectMessage("User not found");
+    }
+
+    @Test
+    public void shouldInsertAUser(){
+        User user = getUser();
+
+        Mockito.when(repository.save(user)).thenReturn(user);
+        service.insert(user);
+    }
+
+    @Test
+    public void shouldNotInsertUserWithSameLogin(){
+
+    }
+
+    @Test
+    public void shouldNotInsertUserWithAExistingEmail(){
+
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setName("Otavio Tarelho");
+        user.setLogin("otaviotarelho");
+        user.setEmail("otaviortdb@gmail.com");
+        user.setPassword("teste123");
+
+        return user;
+    }
 }
